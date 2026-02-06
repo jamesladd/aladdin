57f583a63a085a4a78f3d58ad745fc3440ef0f9b
James Ladd <ladd.james@gmail.com>
2026-02-07 10:40:41 +1100

    Waiting for Office runtime to be available

--- runtime.js
+++ runtime.js
@@ -5,15 +5,21 @@ import {
   initializeAssociations
 } from './addin.js'
 
-// Office.onReady initialization
-Office.onReady((info) => {
-  console.log('Office.onReady called', info)
-
-  if (info.host === Office.HostType.Outlook) {
-    // Initialize associations first
-    initializeAssociations(Office)
-
-    // Then initialize the add-in
-    initializeAddIn(Office)
+// Wait for Office to be available
+function initializeRuntime() {
+  if (typeof Office === 'undefined') {
+    console.error('Office is not defined - Office.js may not be loaded')
+    return
   }
-})
+  // Office.onReady initialization
+  Office.onReady((info) => {
+    console.log('Office.onReady called', info)
+    if (info.host === Office.HostType.Outlook) {
+      initializeAssociations(Office)
+      initializeAddIn(Office)
+    }
+  })
+}
+
+// Call initialization
+initializeRuntime()
